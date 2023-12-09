dfa83d811286205a111598073ab9cf792e242948
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-12-09 18:33:57 -0500

    Wrote an implementation of Merge Sort

---
+++ sortingalgorithms/MergeSort.java
@@ -0,0 +1,57 @@
+package sortingalgorithms;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class MergeSort <T extends Comparable<T>> {
+
+    public List<T> sort(List<T> arr) {
+        int length = arr.size();
+        if (length <= 1) {
+            return arr;
+        }
+        int mid = length / 2;
+        List<T> leftArray = arr.subList(0, mid);
+        List<T> rightArray = arr.subList(mid, length);
+        return merge(sort(leftArray), sort(rightArray));
+    }
+
+    public List<T> merge(List<T> left, List<T> right) {
+        List<T> merged = new ArrayList<T>();
+        int leftPos = 0;
+        int rightPos = 0;
+
+        while (leftPos < left.size() && rightPos < right.size()) {
+            if (left.get(leftPos).compareTo(right.get(rightPos)) < 0) {
+                merged.add(left.get(leftPos));
+                leftPos++;
+            } else {
+                merged.add(right.get(rightPos));
+                rightPos++;
+            }
+        }
+
+        // Add remaining elements from left list
+        if (leftPos < left.size()) {
+            for (int i = leftPos; i < left.size(); i++) {
+                merged.add(left.get(i));
+            }
+        }
+
+        // Add remaining elements from right list
+        if (rightPos < right.size()) {
+            for (int i = rightPos; i < right.size(); i++) {
+                merged.add(right.get(i));
+            }
+        }
+
+        return merged;
+    }
+
+    public static void main (String[] args) {
+        List<Integer> inputArr = List.of(3, 5, 2, 90, 4, 7);
+        MergeSort<Integer> sorter = new MergeSort<Integer>();
+        System.out.println(sorter.sort(inputArr));
+    }
+
+}
