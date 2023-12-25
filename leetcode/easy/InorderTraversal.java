a3bba54e77e6d7fbc8a2a3a79b31401ad82a1486
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-12-25 14:18:00 -0500

    Wrote an iterative solution for the easy leetcode challenge 'Binary Tree Inorder Traversal'

--- leetcode/easy/InorderTraversal.java
+++ leetcode/easy/InorderTraversal.java
@@ -1,8 +1,8 @@
 package leetcode.easy;
 
 import leetcode.TreeNode;
-import java.util.List;
-import java.util.ArrayList;
+
+import java.util.*;
 
 public class InorderTraversal {
 
@@ -26,6 +26,36 @@ public class InorderTraversal {
 
         if (node.right != null) {
             output = helper(node.right, output);
+        }
+
+        return output;
+    }
+
+    public List<Integer> inorderTraversalIterative(TreeNode root) {
+
+        List<Integer> output = new ArrayList<>();
+
+        if (root == null) return output;
+
+        Set<TreeNode> checkedLeft = new HashSet<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode value = stack.peek();
+
+            if (value.left != null && !checkedLeft.contains(value)) {
+                checkedLeft.add(value);
+                stack.push(value.left);
+                continue;
+            }
+
+            output.add(value.val);
+            stack.pop();
+
+            if (value.right != null) {
+                stack.push(value.right);
+            }
         }
 
         return output;
