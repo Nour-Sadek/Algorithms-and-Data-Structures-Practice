35d1dbeb447dd26611979cedf2a456b3d73d4ab7
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-12-25 13:48:25 -0500

    Put the implementations of the data structures ListNode and TreeNode as separate classes in leetcode package

--- leetcode/medium/PathSumII.java
+++ leetcode/medium/PathSumII.java
@@ -1,5 +1,6 @@
 package leetcode.medium;
 
+import leetcode.TreeNode;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -31,21 +32,6 @@ The number of nodes in the tree is in the range [0, 5000].
 -1000 <= targetSum <= 1000
 
  */
-
-// Definition for a binary tree node.
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-         this.val = val;
-         this.left = left;
-         this.right = right;
-     }
-
-}
 
 public class PathSumII {
 
