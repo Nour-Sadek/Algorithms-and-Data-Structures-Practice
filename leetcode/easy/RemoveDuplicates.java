58dc60002bbc934e09d1db4e5c69c5e631e8917a
Nour Sadek <115664745+Nour-Sadek@users.noreply.github.com>
2024-03-14 22:10:32 -0400

    Wrote a straightforward solution for the easy leetcode challenge 'Remove duplicates from sorted array'

---
+++ leetcode/easy/RemoveDuplicates.java
@@ -0,0 +1,64 @@
+package leetcode.easy;
+
+/*
+
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
+The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. 
+The remaining elements of nums are not important as well as the size of nums.
+Return k.
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-100 <= nums[i] <= 100
+nums is sorted in non-decreasing order.
+
+ */
+
+public class RemoveDuplicates {
+
+    public int removeDuplicates(int[] nums) {
+      
+        int[] temp = new int[nums.length];
+
+        int curr = nums[0];
+        temp[0] = curr;
+        int i = 1;
+
+        // Determine the initial values of temp
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != curr) {
+                temp[i] = nums[j];
+                i++;
+                curr = nums[j];
+            }
+        }
+
+        // Assign values of temp to num
+        for (int k = 0; k < i; k++) {
+            nums[k] = temp[k];
+        }
+
+        return i;
+
+    }
+  
+}
