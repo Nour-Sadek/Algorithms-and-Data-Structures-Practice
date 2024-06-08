1a81cd76caf4b8ceee553163bb159a4d762f0ec7
Nour Sadek <115664745+Nour-Sadek@users.noreply.github.com>
2024-06-08 09:15:18 -0400

    Wrote a solution to the easy leetcode challenge "Remove Linked List Elements"

---
+++ leetcode/easy/RemoveLinkedListElements.java
@@ -0,0 +1,58 @@
+package leetcode.easy;
+
+import leetcode.ListNode;
+
+/*
+
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+Example 1:
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+
+Constraints:
+
+- The number of the nodes in the list is in the range [0, 10^4].
+- 1 <= Node.val <= 50
+- 0 <= val <= 50
+
+ */
+
+public class RemoveLinkedListElements {
+
+    public ListNode removeElements(ListNode head, int val) {
+
+        if (head == null) return null;
+        
+        ListNode curr = head;
+        ListNode previous = null;
+
+        while (curr != null) {
+            if (curr.val != val) {
+                previous = curr;
+                curr = curr.next;
+            } else if (curr.val == val && previous == null) {
+                head = curr.next;
+                curr = head;
+            } else {
+                previous.next = curr.next;
+                curr = curr.next;
+            }
+        }
+
+        return head;
+        
+    }
+
+}
